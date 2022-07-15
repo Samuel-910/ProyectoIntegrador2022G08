22b29c02ee25b4c27f440944de37899911ba50e7
Samuel_910 <dasarupazfesandra@gmail.com>
2022-07-15 14:44:56 -0500

    Proyecto

--- SysCafeteriaLosChuquis/src/main/java/pe/edu/upeu/dao/DetalleProductoDao.java
+++ SysCafeteriaLosChuquis/src/main/java/pe/edu/upeu/dao/DetalleProductoDao.java
@@ -20,7 +20,7 @@ public class DetalleProductoDao extends AppCrud {
     public void crearDetalleProducto() {
         leerA = new LeerArchivo(TABLA_DETALLEPRODUCTO);
         dpTo = new DetalleProductoTO();
-        String idDPro = generarId(leerA, 0, "dP", 1);
+        String idDPro = generarId(leerA, 0, "DP", 1);
         dpTo.setIdDProducto(idDPro);
         dpTo.setNombreDProd(leerT.leer("", "Nombre Producto"));
         mostrarCategoria();
